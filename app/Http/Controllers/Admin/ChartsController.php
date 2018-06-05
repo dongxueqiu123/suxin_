83e956781b223910c815531fed7c05da61db4d5b
344748243@qq.com <dongxue7512959>
2018-06-05 16:33:59 +0800

    增加查询

--- app/Http/Controllers/Admin/ChartsController.php
+++ app/Http/Controllers/Admin/ChartsController.php
@@ -7,6 +7,7 @@
  */
 namespace App\Http\Controllers\Admin;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Services\CollectorsServices;
 use Illuminate\Support\Facades\Auth;
@@ -91,7 +92,6 @@ class ChartsController extends Controller{
             ]
         );
     }
-
 
     public function collectorChartRealTime($id){
         $queryArray['firmId'] = Auth::user()->company->id??'';
@@ -189,6 +189,68 @@ class ChartsController extends Controller{
         return $data;
     }
 
+    public function getHistorySpeedData($http_type,$state,$id,$endDate,$period){
+
+        $_SERVER['HTTP_HOST'] = 'www.suxiniot.com';
+        $url = $http_type.$_SERVER['HTTP_HOST'].'/console/influx/timeseries/period/'.$state.'/'.$id.'?endTime='.($endDate-8*60*60)*1000 .'&period='.$period;
+        $data = $this->httpGet($url,[]);
+        return $data;
+    }
+
+    public function collectorHistoryChart($id,Request $request){
+        $queryArray['firmId'] = Auth::user()->company->id??'';
+
+        $startTime = $request->input('startTime');
+        $endTime   = $request->input('endTime');
+        $ext['sort'] = true;
+        $collectors = $this->collectorsServices->getList(0,$queryArray,'','',$ext);
+
+        $collector =  ($id==0)?$collectors->first():$this->collectorsServices->get($id);
+
+        return view('charts.collectorHistoryChart',
+            [
+                'time'=>$endTime,
+                'collector'=>$collector,
+                'collectors'=>$collectors,
+                'boxTitle'=>'无线节点数据展示',
+                'active' => 'collectors',
+            ]
+        );
+    }
+
+    /**
+     * 获取请求时间段和次数
+     * 1.5分钟之内直接请求
+     * 2.5-30分钟每次请求5分钟
+     * 3.30-60分钟每次请求10分钟
+     * 4.60-240分钟每次请求30分钟
+     * 5.大于240分钟每次请求60分钟
+     * @param $startTime 开始时间
+     * @param $endTime 结束时间
+     * @return array
+     */
+    public function countIntervalAndTimes($startTime,$endTime){
+        $differenceTime = $endTime-$startTime;
+
+        if($differenceTime <= 300){ //5分钟之内直接请求
+            $interval = (int)ceil($differenceTime/60);//分钟
+        }elseif($differenceTime > 300 && $differenceTime <= 30*60){ //5-30分钟每次请求5分钟
+            $times = (int) ceil($differenceTime/(60*5));//次数
+            $interval = 5;//分钟
+        }elseif($differenceTime > 30*60 && $differenceTime <= 60*60){ //30-60分钟每次请求10分钟
+            $times = (int) ceil($differenceTime/(60*10));
+            $interval = 10;
+        }elseif($differenceTime > 60*60 && $differenceTime <= 60*240){ //60-240分钟每次请求30分钟
+            $times = (int) ceil($differenceTime/(60*30));
+            $interval = 30;
+        }elseif($differenceTime > 60*240 ){ //大于240分钟每次请求60分钟
+            $times = ceil($differenceTime/60*60);
+            $interval = 60;
+        }
+        return ['interval'=>$interval??1,'times'=>$times??1];
+    }
+
+
     private function httpGet($url,$params = array()) {
         try{
             $html = @file_get_contents($url);
@@ -198,6 +260,32 @@ class ChartsController extends Controller{
 
         $collectorData = json_decode($html,true);
         return $collectorData;
+    }
+
+    public function  collectorHistoryResponse(Request $request){
+        ini_set('date.timezone','Asia/Shanghai');
+        $queryArray['firmId'] = Auth::user()->company->id??'';
+        $startTime = strtotime($request->input('startTime'));
+        $endTime   = strtotime($request->input('endTime'),time());
+        $id   = $request->input('id');
+        //$startTime = time()-3*60;
+        $http_type = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')) ? 'https://' : 'http://';
+        //$speedData = $this->getSpeedData($http_type,'acc_orig',$id,$startTime);
+        //dump($speedData);
+
+        $IntervalAndTimes = $this->countIntervalAndTimes($startTime, $endTime);
+        $data = [];
+
+        for ($i = 0; $i<$IntervalAndTimes['times'];$i++){
+            $speedData = $this->getHistorySpeedData($http_type,'acc_orig',$id??'',$endTime-$i*$IntervalAndTimes['interval']*60,$IntervalAndTimes['interval']);
+            $data =array_merge($speedData['data'],$data);
+        }
+
+
+        return response()->json([
+            'code' => 0,
+            'data' => json_encode($data)??''
+        ]);
     }
 
     public function  collectorResponse(){
